a51217a67648ae226f45879c2ea658d7a3348713
willot <vianney.willot@gmail.com>
2015-01-23 17:11:51 -0600

    test model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe User, :type => :model do
     it { expect(user).to validate_uniqueness_of(:username) }
     it { expect(user).to validate_uniqueness_of(:email) }
   end
+
+  describe "password length" do
+    it { expect(user).to ensure_length_of(:password).is_at_most(20).with_message("You are limited to 20 characters")}
+    it { expect(user).to ensure_length_of(:password).is_at_least(6).with_message("You need at least 6 characters")}
+  end
+
+  describe "bio lenght" do
+    it { expect(user).to ensure_length_of(:bio).is_at_most(2000).with_message("You are limited to 2000 characters")}
+  end
+
+  describe " use secure password" do
+    it { expect(user).to have_secure_password}
+  end
+
 end
