447dce17882272577bc900fcd21a6fbb242bc624
willot <vianney.willot@gmail.com>
2015-01-23 11:31:28 -0600

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+      user = User.find_by(username: params[:username])
+
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        render :new
+      end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
