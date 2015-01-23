e42e2cfffcba80bc0c2484231779b9098e4d81c2
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-23 13:55:12 -0600

    Initial Trip associations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
 class Trip < ActiveRecord::Base
+  belongs_to :origin, class_name: "Location"
+  belongs_to :destination, class_name: "Location"
+  belongs_to :creator, class_name: "User"
+
+  has_many :travelers
+  has_many :users, through: :travelers
+
   validates :origin, presence: true
   validates :destination, presence: true
   validates :start_at, presence: true
