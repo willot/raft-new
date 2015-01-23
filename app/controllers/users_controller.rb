a3fc5e4e31bddb1dde8778488c1583e697e7e83a
willot <vianney.willot@gmail.com>
2015-01-23 10:37:16 -0600

    can create a user and be directed to their page, or render form if failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user= User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "You created an account"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :age, :email, :bio, :guide, :password)
   end
 
 end
