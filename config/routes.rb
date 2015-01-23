b39ed27085681614dde807d6f4679d6ebf818d7b
willot <vianney.willot@gmail.com>
2015-01-23 10:54:59 -0600

    fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :users
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  resources :users
 
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
