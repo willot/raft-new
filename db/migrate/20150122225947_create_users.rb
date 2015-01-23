67910ff9b84f358d886649ea9c58320d87818383
willot <vianney.willot@gmail.com>
2015-01-22 19:01:53 -0600

    correct typo in user migration

--- db/migrate/20150122225947_create_users.rb
+++ db/migrate/20150122225947_create_users.rb
@@ -3,12 +3,13 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :user_name
+      t.string :username
       t.integer :age
       t.string :email
       t.string :password_digest
       t.text :bio
       t.boolean :guide ||= false
+      references :location
 
       t.timestamps null: false
     end
