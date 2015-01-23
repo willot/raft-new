fe2cfc55eb046b78530988b8a3411f032c1eda40
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-23 11:23:50 -0600

    initial search results table

---
+++ db/migrate/20150123171146_create_search_results.rb
@@ -0,0 +1,12 @@
+class CreateSearchResults < ActiveRecord::Migration
+  def change
+    create_table :search_results do |t|
+      t.references :user
+      t.integer    :budget
+      t.datetime   :start_at
+      t.datetime   :end_at
+
+      t.timestamps null: false
+    end
+  end
+end
