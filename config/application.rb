5d692e2724b981a0c7d8ee9943fe02e5483762c4
Nodira Berdieva & willot <pair+nberdieva.willot@devbootcamp.com>
2015-01-22 20:06:18 -0600

    add twitter meta tags to layout

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require 'rspec/core/rake_task'
+require "rspec/core/rake_task"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
