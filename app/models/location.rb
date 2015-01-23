5a648ba468c2185434807cbd155e254be56439a4
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-23 14:21:36 -0600

    initial location associations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,13 @@
 class Location < ActiveRecord::Base
+  has_many :trips_origins, class_name: "Trip", foreign_key: "origin_id"
+  has_many :trip_destinations, class_name: "Trip", foreign_key: "destination_id"
+
+  has_many :travelers, through: :trips_origins
+  has_many :users, through: :travelers
+
+  has_many :search_result_locations
+  has_many :search_results, through: :search_result_locations
+
   validates :city, presence: true
   validates :country, presence: true
 end
