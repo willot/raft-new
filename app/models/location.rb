63c2f46afe91414f1105aa5e2f20a8d0a318892c
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-23 19:02:49 -0600

    fixed typo in trip origin association

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  has_many :trips_origins, class_name: "Trip", foreign_key: "origin_id"
+  has_many :trip_origins, class_name: "Trip", foreign_key: "origin_id"
   has_many :trip_destinations, class_name: "Trip", foreign_key: "destination_id"
 
   has_many :travelers, through: :trips_origins
