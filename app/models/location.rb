6c9443041193645e94fd1cb95b5c77f50cf6e060
Nodira Berdieva <nberdieva@gmail.com>
2015-01-24 13:06:56 -0600

    rename locate method in location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
+require 'openssl'
+require 'geokit'
+
 class Location < ActiveRecord::Base
   validates :city, presence: true
   validates :country, presence: true
+
+  def locate(ip)
+    a = Geokit::Geocoders::MultiGeocoder.geocode(ip)
+    @city = a.city
+  end
 end
