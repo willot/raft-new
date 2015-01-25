83691cfb361f78960a25ef036ea39f5a9b83860d
willot <vianney.willot@gmail.com>
2015-01-25 15:40:48 -0600

    Query take date that user type in the form

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,4 +1,5 @@
 require 'momondo/client.rb'
+require 'Date'
 
 class SearchResultsController < ApplicationController
   def index
@@ -13,8 +14,9 @@ class SearchResultsController < ApplicationController
 
   def create
    @search_result = SearchResult.new(search_result_params)
+   airport_code = convert_city_to_airport(@search_result.current_city.downcase)
    @client = Momondo::Client.new
-   @results = @client.where_to_go(leave_date: '05/07/2015',leave_from: "LAX", max_price: @search_result.budget )
+   @results = @client.where_to_go(leave_date: @search_result.start_at.to_s ,leave_from: airport_code, max_price: @search_result.budget )
 
    render "search_results/index"
   end
@@ -23,6 +25,10 @@ class SearchResultsController < ApplicationController
 
   def search_result_params
    params.require(:search_result).permit(:current_city, :budget, :start_at, :end_at)
+  end
+
+  def convert_city_to_airport(current_city)
+   Location.find_by(city: current_city).airports.first.code
   end
 end
 
