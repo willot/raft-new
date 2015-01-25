a5365c9800b74fd5c076b985cc0cd4082169d87b
willot <vianney.willot@gmail.com>
2015-01-25 12:24:14 -0600

    comment avatar code to pass migration need to be fixed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],        # required
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"]                  # required
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     :provider               => 'AWS',                        # required
+#     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],        # required
+#     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]        # required
+#   }
+#   config.fog_directory  = ENV["AWS_BUCKET"]                  # required
 
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage           = :file
-    config.enable_processing = false
-    config.root              = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
+#   # For testing, upload files to local `tmp` folder.
+#   if Rails.env.test? || Rails.env.cucumber?
+#     config.storage           = :file
+#     config.enable_processing = false
+#     config.root              = "#{Rails.root}/tmp"
+#   else
+#     config.storage = :fog
+#   end
 
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-end
+#   config.cache_dir = "#{Rails.root}/tmp/uploads"
+# end
