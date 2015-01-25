f292613f74a5bea0244bfc3abf1152221da38a76
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-24 19:19:37 -0600

    current_user working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def authenticate_user
+    redirect_to :back, alert: 'You are not logged in. Shame.' unless current_user
   end
 end
