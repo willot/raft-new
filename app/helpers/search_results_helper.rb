9f360c2df266fea155914314dd40cdebbc9ae768
willot <vianney.willot@gmail.com>
2015-01-25 12:26:42 -0600

    create method to convert code into city fir result page"

--- app/helpers/search_results_helper.rb
+++ app/helpers/search_results_helper.rb
@@ -1,2 +1,9 @@
 module SearchResultsHelper
+  def convert_airport_to_city(code)
+    @destination = Airport.find_by(code: code).location.city
+  end
+
+  def convert_city_to_airport(current_city)
+    Location.find_by(city: current_city).airport.code
+  end
 end
