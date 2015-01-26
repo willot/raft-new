134bc15a0658fb61d507fb4d269b9f1a35b5abcf
Nodira Berdieva <nberdieva@gmail.com>
2015-01-25 23:26:46 -0600

    add a route for guide index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get '/search_results' => 'search_results#index'
   post '/search_results' => 'search_results#create'
 
+  get '/guides' => 'guides#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
