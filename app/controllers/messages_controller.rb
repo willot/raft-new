d6562656cd6cdf0ab5a652ea4bd9189c42b2ea16
willot <vianney.willot@gmail.com>
2015-01-27 10:13:27 -0600

    modify the controller and form so we can create a message for user and trip

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,8 @@
 class MessagesController < ApplicationController
+  before_action :check_if_trip
+  before_action :set_receiver
+
+
   def index
     @user = current_user
     @received = @user.received_messages
@@ -7,20 +11,37 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
-    @user = User.find(params[:user_id])
   end
 
   def create
     @message = Message.new(message_params)
-    @message.creator = current_user
-    @receiver = User.find(params[:user_id])
-    @message.recipient = @receiver
-    @message.save
-
-    redirect_to @receiver, notice: "Your message has been sent!"
+    if @message.save
+      if @trip
+        redirect_to @trip, notice: "Your message has been sent!"
+      else
+        redirect_to @receiver, notice: "Your message has been sent!"
+      end
+    else
+      render 'new'
+    end
   end
 
+private
+
+  def check_if_trip
+    @trip = Trip.find(params[:trip_id]) if params[:trip_id]
+  end
+
+  def set_receiver
+    @receiver = User.find(params[:user_id]) if params[:user_id]
+  end
+
+
   def message_params
-    params.require(:message).permit(:title, :content, :wrote_at)
+    if @trip
+      params.require(:message).permit(:title,:content, :wrote_at).merge(messageable_id: @trip.id, messageable_type: 'Trip', creator_id: current_user.id)
+    else
+      params.require(:message).permit(:title,:content, :wrote_at).merge(messageable_id: @receiver.id, messageable_type: 'User', creator_id: current_user.id)
+    end
   end
 end
