d5a345fc733a0c8d51cdfbbc9002e25abcbf74db
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-26 20:19:42 -0600

    google map added, early experimentation phase

--- app/assets/javascripts/guides.js
+++ app/assets/javascripts/guides.js
@@ -1,37 +1,43 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// (function(window, google) {
 
-//   //map options
-//   var options = {
-//     center: {
-//       lat: 41.8369,
-//       lng: 87.6847
-//     },
-//     zoom: 10
-//   }
-//   element = document.getElementById('map-canvas'),
-//   //map
-//   map = new google.maps.Map(element, options);
-// }(window, google));
+  function initialize() {
+    var mapOptions = {
+      zoom: 1,
+      center: new google.maps.LatLng(-34.397, 150.644),
+    };
 
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions)
 
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
+    map.addListener('click', function(e){
+          alert('click');
+          console.log(e);
+          console.log(this);
+        });
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
+    var marker = new google.maps.Marker({
+      position: {
+        lat: 41.8369,
+        lng: -87.6847
+      },
+     map: map
+    });
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-}
+    marker.info = new google.maps.InfoWindow({
+      content: '<b> JAMAZING! </b>'
+    });
 
-window.onload = loadScript;
+    google.maps.event.addListener(marker, 'click', function() {
+      marker.info.open(map, marker);
+    })
+  }
+
+  function loadScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+        'callback=initialize';
+    document.body.appendChild(script);
+  }
+  window.onload = loadScript;
