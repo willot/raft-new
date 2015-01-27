59d30d81082e00d8c6da5a83ced4f8b007e03ba6
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-26 20:20:45 -0600

    set up guided_cities method for map functionality

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,14 +1,31 @@
 class GuidesController < ApplicationController
   def index
     @guides = User.where(guide: true)
+
+    # respond_to do |format|
+    #   format.json { render json: @guides}
+    # end
   end
 
   def new
     @guide = User.find(session[:user_id])
   end
 
+
+  private
+
   def find_guides
     @loc = Location.find_by(city: params[:city])
     @guides = @loc.guides
+  end
+
+  def guided_cities
+    guided_cities = []
+
+    Location.all.each do |location|
+      if location.guides.count != 0
+        guided_cities << location
+      end
+    end
   end
 end
