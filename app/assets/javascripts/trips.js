1f875df8a57a4d0729dc0308e119cfaffb4ad92e
Nodira Berdieva <nberdieva@gmail.com>
2015-01-29 11:17:21 -0600

    fix map on trip page

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -5,20 +5,22 @@
 $(document).ready(function() {
   var showMap = function(data) {
     L.mapbox.accessToken = 'pk.eyJ1IjoiamFtYXppbmciLCJhIjoiWm5QaUdZZyJ9.A91w3rJWJ3XWcuyz7kW8GA';
-    var map = L.mapbox.map('tripmap', 'jamazing.l2jlo2pp', {zoomControl: false}).setView([40, -74.50], 2);
+    var map = L.mapbox.map('tripmap', 'jamazing.l2jlo2pp', {zoomControl: false}).setView([40, -74.50], );
 
     map.dragging.disable();
     map.touchZoom.disable();
     map.doubleClickZoom.disable();
     map.scrollWheelZoom.disable();
 
-    if (map.tap) map.tap.disable();
+    if (map.tap) { map.tap.disable(); }
 
     map.featureLayer.on('ready', function(e) {
       var line = data;
+      debugger;
+
       var polyline_options = {color: '#000'};
       var polyline = L.polyline(line, polyline_options).addTo(map);
     });
   }
-  showMap($.parseJSON($('script#coord').text()));
+  showMap(window.coords);
 });
