8f9e990e602011769f6f1738aa7d3d80aede7a20
Nodira Berdieva <nberdieva@gmail.com>
2015-01-29 13:14:15 -0600

    lots of tweaks for the landing page

--- app/assets/javascripts/mapbox.js
+++ app/assets/javascripts/mapbox.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     success: function(data){
       geojson = $.parseJSON(data)
       L.mapbox.accessToken = 'pk.eyJ1IjoiamFtYXppbmciLCJhIjoiWm5QaUdZZyJ9.A91w3rJWJ3XWcuyz7kW8GA';
-      window.map = L.mapbox.map('map', 'jamazing.l26m1gnb', {maxBounds: [[20.0, 0.0]], maxZoom: 3, minZoom: 3}).setView([20.0, 0], 3);
+      window.map = L.mapbox.map('map', 'jamazing.l26m1gnb', {maxBounds: [[20.0, 0.0]], maxZoom: 2, minZoom: 2}).setView([20.0, 0], 2);
       window.map.featureLayer.setGeoJSON(geojson);
     }
   });
