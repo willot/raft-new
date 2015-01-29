fc944fd15f0af427d07cbd4b2cdfb9eff47a2f0a
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-29 09:44:24 -0600

    repositioned map

--- app/assets/javascripts/mapbox.js
+++ app/assets/javascripts/mapbox.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     success: function(data){
       geojson = $.parseJSON(data)
       L.mapbox.accessToken = 'pk.eyJ1IjoiamFtYXppbmciLCJhIjoiWm5QaUdZZyJ9.A91w3rJWJ3XWcuyz7kW8GA';
-      var map = L.mapbox.map('map', 'jamazing.l26m1gnb', {maxBounds: [[-40.0, 70.0]], maxZoom: 50, minZoom: 3}).setView([40, -74.50], 1);
+      var map = L.mapbox.map('map', 'jamazing.l26m1gnb', {maxBounds: [[-50.0, 70.0]], maxZoom: 50, minZoom: 3}).setView([40, -74.50], 1);
       map.featureLayer.setGeoJSON(geojson);
     }
   });
