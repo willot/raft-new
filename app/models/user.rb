ec976a9c54d1ff5ba14949e2ca9e30f242121ef0
Nodira Berdieva <nberdieva@gmail.com>
2015-01-29 17:27:58 -0600

    remove email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   has_many :messages, as: :messageable
 
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true
+  # validates :email, uniqueness: true, presence: true
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :first_name, presence: true
   validates :last_name, presence: true
