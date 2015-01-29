2c64e9e3fa50ff408d3e353f0575835baacbdc95
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-01-28 18:59:47 -0600

    took off interaction of map on trip show page

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -5,11 +5,16 @@
 $(document).ready(function() {
   var showMap = function(data) {
     L.mapbox.accessToken = 'pk.eyJ1IjoiamFtYXppbmciLCJhIjoiWm5QaUdZZyJ9.A91w3rJWJ3XWcuyz7kW8GA';
-    var map = L.mapbox.map('tripmap', 'jamazing.l2jlo2pp',
-              {maxBounds: [[30.0,-85.0],[50.0,-65.0]], maxZoom: 50, minZoom: 3}).setView([40, -74.50], 9);
+    var map = L.mapbox.map('tripmap', 'jamazing.l2jlo2pp', {zoomControl: false}).setView([40, -74.50], 2);
+
+    map.dragging.disable();
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+
+    if (map.tap) map.tap.disable();
 
     map.featureLayer.on('ready', function(e) {
-      debugger;
       var line = data;
       var polyline_options = {color: '#000'};
       var polyline = L.polyline(line, polyline_options).addTo(map);
